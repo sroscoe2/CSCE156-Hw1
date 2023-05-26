0f3fc81e5af76e126229d07567fef4688f6d496d
sroscoe2 <51931272+sroscoe2@users.noreply.github.com>
2023-05-26 14:54:14 -0500

    Add files via upload

---
+++ Homework1/src/binaryArray/BinaryArray.java
@@ -0,0 +1,33 @@
+package binaryArray;
+
+public class BinaryArray {
+	//TODO: have fields
+
+	//BinaryArray Constructor
+	public BinaryArray (String s) {
+		//TODO: implement constructor
+		//IllegalArgumentException if s is invalid
+	}
+
+	@Override
+	public String toString() {
+		//TODO: implement method
+		return "";
+	}
+	//getter method that retuns value at a specific index
+	public char get(int idx) {
+		//TODO: implement get method
+		return 'z';
+	}
+	//setter function
+	public void set(int idx, char value) {
+		//TODO: implement set method
+
+	}
+	//gets size of binaryArray
+	public int size() {
+		//TODO: implement size method
+		return -1;
+	}
+
+}
---
+++ Homework1/src/binaryArray/BinaryArrayDriver.java
@@ -0,0 +1,26 @@
+package binaryArray;
+
+public class BinaryArrayDriver {
+
+	public static void main(String[] args) {
+		// TODO: implement driver
+
+		// prompts include:
+		// Please enter a binary string:
+		// Choose what you want to do: 1) print current BinaryArray 2) get a value 3)
+		// set a value 4) view size 5) exit:
+		// option 1
+		// This binary Array has X variables: [binary array here]
+		// option 2
+		// Input the position you want to see:
+		// Position is out of range!! Re-Input the position you want to see:
+		// The element at 2 is X
+		// option 3
+		// Input the position you want to change and the value you want to assign to it:
+		// option 4
+		// The size of this binary array is: X
+		// option 5
+		// Goodbye
+	}
+
+}
